1138ab0f159331b461b71cdf59d939be60b8c0db
Yifan li <779793332@qq.com>
2015-01-15 00:17:09 +0800

    Update excelSheetColumnNumber.cpp
    delete the isAlper function

--- excelSheetColumnNumber.cpp
+++ excelSheetColumnNumber.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    int isNotAlper(char in)
-    {
-        if(in > 'Z' || in < 'A') return 1;
-        return 0;
-    }
-    int titleToNumber(string s) { 
+    int titleToNumber(string s) {  
         int len = s.length();
         if(0 == len) return -1;
         int sum = 0;
         for(int i = 0; i < len; i++)
         {
-            if(isNotAlper(s.at(i))) return -1;
-            sum *= 26;
-            sum += s.at(i) - 'A' + 1;
+            int columnCharNum = s.at(i);
+            if(columnCharNum > 'Z' || columnCharNum < 'A') return -1;
+            sum = sum*26 + columnCharNum - 'A' + 1;
         }
-        return sum;
+        return sum; 
     }
 };
