02a42c699cee668fa720bf5bde9ad3047b11ddec
Yifan li <779793332@qq.com>
2015-02-02 12:11:24 +0800

    Update RemoveDuplicatesfromSortedArray.cpp

--- CppSrc/RemoveDuplicatesfromSortedArray.cpp
+++ CppSrc/RemoveDuplicatesfromSortedArray.cpp
@@ -3,12 +3,10 @@ public:
     int removeDuplicates(int A[], int n) {
         if(n <= 1) return n;
         int pos = 0;
-        for(int i = 0; i < n-1; i++) {
-            if(A[i] != A[i+1]) {
-                pos++;
-                A[pos] = A[i+1];
-            }
+        for(int searchIndex = 0; searchIndex < n-1; searchIndex++) {
+            if(A[searchIndex] != A[searchIndex+1])
+                A[++pos] = A[searchIndex+1];
         }
-        return pos + 1;
+        return ++pos;
     }
 };
