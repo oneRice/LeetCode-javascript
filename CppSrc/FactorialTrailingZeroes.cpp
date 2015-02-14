7d1662e2173ee12ca0d4c5f1833b83f1b7b0e12d
Yifan li <779793332@qq.com>
2015-02-14 23:31:09 +0800

    Update FactorialTrailingZeroes.cpp

--- CppSrc/FactorialTrailingZeroes.cpp
+++ CppSrc/FactorialTrailingZeroes.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int fiveContainer = 5;
+        long int fiveContainer = 5;
         int sum = 0;
         while(fiveContainer <= n) {
             sum += n/fiveContainer;
-            if(fiveContainer == 1220703125) {
-                break;
-            }
             fiveContainer *= 5;
         }
         return sum;  
