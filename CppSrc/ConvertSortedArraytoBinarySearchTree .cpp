28bca114b7bb918b30e4a0ff99f29026dc82d824
Yifan li <779793332@qq.com>
2015-04-21 00:00:01 +0800

    Update ConvertSortedArraytoBinarySearchTree .cpp

--- CppSrc/ConvertSortedArraytoBinarySearchTree .cpp
+++ CppSrc/ConvertSortedArraytoBinarySearchTree .cpp
@@ -9,29 +9,15 @@
  */
 class Solution {
 public:
-    vector <int> getSection(std::vector<int> &num, int start, int end) {
-        std::vector<int> nu;
-        if(end < start)
-            return nu;
-        if(end == start) {
-            nu.push_back(num[start]);
-            return nu;
-        }
-        std::vector<int>::iterator startIter = num.begin() + start;
-        std::vector<int>::iterator endIter = num.begin() + end + 1;
-        std::vector<int> v(startIter, endIter);
-        return v;
-    }
-
     TreeNode *sortedArrayToBST(vector<int> &num) {
         if(num.empty())
             return NULL;
         int size = num.size();
         int mid = size / 2;
         TreeNode* root = new TreeNode(num[mid]);
-        vector<int> left = getSection(num, 0, mid - 1);
+        vector<int> left(num.begin()+0, num.begin()+mid);
         root->left = sortedArrayToBST(left);
-        vector<int> right = getSection(num, mid + 1, size - 1);
+        vector<int> right(num.begin()+mid+1, num.begin()+size);
         root->right = sortedArrayToBST(right);
         return root;
     }
